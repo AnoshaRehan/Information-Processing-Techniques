b1f6e182358890ff5e5e6374e0ec705825bfa2ce
Anosha Rehan <45163583+AnoshaRehan@users.noreply.github.com>
2020-04-05 00:15:36 +0500

    Added files for Assignment01Q6

---
+++ Assignment01Q6/Q6/k163833_Q6/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k163833_Q6
+{
+    public class Bubble_Sort
+    {
+
+        public void bubbleSort(int[] arr, int size)
+
+        {
+            bool pass = true;
+            unsafe
+            {
+                int *temp;
+
+                for (int i = 1; (i <= (size - 1)) && pass; i++)
+                {
+                    pass = false;
+                    for (int j = 0; j < (size - 1); j++)
+                    {
+                        if (arr[j+1] > arr[j])
+                        {
+                            temp = (int*)arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = (int)temp;
+                            pass = true;
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+}
