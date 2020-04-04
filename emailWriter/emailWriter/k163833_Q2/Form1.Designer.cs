6a29a01be41bf6c34b766a9fef27f66e5170489f
Anosha Rehan <45163583+AnoshaRehan@users.noreply.github.com>
2020-04-05 00:24:05 +0500

    Added files for Assignment01Q2

---
+++ emailWriter/emailWriter/k163833_Q2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace k163833_Q2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toBox = new System.Windows.Forms.TextBox();
+            this.subjectBox = new System.Windows.Forms.TextBox();
+            this.bodyBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Subject";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Body";
+            // 
+            // toBox
+            // 
+            this.toBox.Location = new System.Drawing.Point(126, 15);
+            this.toBox.Name = "toBox";
+            this.toBox.Size = new System.Drawing.Size(100, 20);
+            this.toBox.TabIndex = 3;
+            // 
+            // subjectBox
+            // 
+            this.subjectBox.Location = new System.Drawing.Point(126, 63);
+            this.subjectBox.Name = "subjectBox";
+            this.subjectBox.Size = new System.Drawing.Size(100, 20);
+            this.subjectBox.TabIndex = 4;
+            this.subjectBox.TextChanged += new System.EventHandler(this.subjectBox_TextChanged);
+            // 
+            // bodyBox
+            // 
+            this.bodyBox.Location = new System.Drawing.Point(126, 128);
+            this.bodyBox.Multiline = true;
+            this.bodyBox.Name = "bodyBox";
+            this.bodyBox.Size = new System.Drawing.Size(174, 78);
+            this.bodyBox.TabIndex = 5;
+            this.bodyBox.TextChanged += new System.EventHandler(this.bodyBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bodyBox);
+            this.Controls.Add(this.subjectBox);
+            this.Controls.Add(this.toBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox toBox;
+        private System.Windows.Forms.TextBox subjectBox;
+        private System.Windows.Forms.TextBox bodyBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ emailWriter/emailWriter/k163833_Q2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace k163833_Q2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bodyBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subjectBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            EmailMessage mail = new EmailMessage();
+            string to = toBox.Text;
+            mail.Email(subjectBox.Text, to, bodyBox.Text);
+            string xmlFile = "c:\\users\\New\\Desktop\\k163833\\k163833_Q2\\test2.xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(EmailMessage));
+            TextWriter writer = new StreamWriter(xmlFile);
+            serializer.Serialize(writer, mail);
+            writer.Close();
+        }
+
+        [Serializable]
+        public class EmailMessage
+        {
+            public string To;
+            public String Subject;
+            public String MessageBody;
+            public void Email(string _subject, string _to, string body)
+            {
+                To = _to;
+                Subject = _subject;
+                MessageBody = body;
+                string filename = ConfigurationManager.AppSettings.Get("xmlFile");
+                MailMessage msgMail;
+                msgMail = new MailMessage();
+                msgMail.To.Add(To);
+                msgMail.Subject = Subject;
+                msgMail.Body = MessageBody;
+                msgMail.Dispose();
+            }
+        }
+    }
+}
